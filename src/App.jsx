87299a232068b6163d7514392597ee96d74e0f64
Mubashir Ali <mubashirali@pop-os.localdomain>
2023-10-16 17:49:22 +0500

    Created routes for the Esate app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Router, Route, Routes} from "react-router-dom";
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign-in" element={<Signin />}/>
+      <Route path="/sign-up" element={<Signup />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }

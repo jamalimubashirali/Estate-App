ad06ab6d5f9ffc76fefc347a59682965515f2b64
Mubashir Ali <jamalimubashirali@gmail.com>
2023-10-17 18:19:33 +0500

    Established Connection With Database

--- api/index.js
+++ api/index.js
@@ -12,5 +12,5 @@ mongoose.connect(process.env.MONGOS).then(() => {
 
 const app = express();
 app.listen(3000 , () =>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000!!");
 });

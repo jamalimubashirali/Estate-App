d6f1be0030cc84e8b7fa721d996129a97395ae6c
Mubashir Ali <jamalimubashirali@gmail.com>
2023-10-23 23:56:34 +0500

    Created Model Named User

---
+++ api/models/user_model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userEmail: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userPassword: {
+        type: String,
+        required: true,
+    },
+},{timestamps : true});
+
+const User = mongoose.model('User' , userSchema);
+
+export default User;

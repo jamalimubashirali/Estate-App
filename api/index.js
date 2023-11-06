498525e631577bf18238a87a9ff767b2e224587f
Mubashir Ali <jamalimubashirali@gmail.com>
2023-11-06 16:29:11 +0500

    Created a user api router

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routers/user_router.js'
 
 dotenv.config();
 
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3000 , () =>{
     console.log("Server is running on port 3000!!");
 });
+
+app.use('/api/user' , userRouter);

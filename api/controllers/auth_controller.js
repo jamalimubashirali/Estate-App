b21863b45a66e5b7d2ece24dac2f648148df8cf6
Mubashir Ali <jamalimubashirali@gmail.com>
2023-11-22 21:43:40 +0500

    Solved Confilicting problem arsing in database

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -4,7 +4,7 @@ import bcryptjs from 'bcryptjs';
 export const signup = async (req, res) => {
     const { username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password , 10);
-    const newUser = new User({username , email , password});
+    const newUser = new User({username , email , password:hashedPassword});
     await newUser.save();
     res.status(201).json('User Successfully Created');
 };

8fa0d503c308bd2ef924e8fcc9ac2f088906ac71
Mubashir Ali <jamalimubashirali@gmail.com>
2024-01-07 13:01:36 +0500

    Made Sign In page UI

--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Signin() {
+export default function 
+() {
   return (
-    <div>Signin</div>
+    <div className = "p-3 max-w-lg mx-auto">
+      <h1 className='text-3xl my-10 font-semibold text-center'>Sign In</h1>
+      <form action="#" className='flex flex-col gap-4'>
+          <input type="email" name="email" id="email" placeholder='Email' className='p-3 border rounded-lg'/>
+          <input type="password" name="password" id="password" placeholder='Password' className='p-3 border rounded-lg'/>
+          <button className='bg-blue-500 text-white rounded-lg p-3 hover:opacity-90 disabled:opacity-70 uppercase font-semibold'>Sign In</button>
+          <button className='bg-red-700 text-white rounded-lg p-3 hover:opacity-90 disabled:opacity-70 uppercase font-semibold'>Continue With Google</button>
+      </form>
+      <div className='flex my-2 gap-2'>
+          <p>Don't Have an Account?</p>
+          <Link to={'/sign-up'}>
+          <span className='text-blue-800' >
+            Create an Account. 
+          </span>
+              </Link>
+      </div>
+    </div>
   )
 }
--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Signup() {
+  const [formData , setFormData] = useState({})
+  const handleChange = (e) => {
+    setFormData({
+        ...formData,
+        [e.target.id] : e.target.value,
+    });
+  };
+  console.log(formData)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('http://localhost:3000/api/auth/signup', 
+    {
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json',
+      },
+      body : JSON.stringify(formData),
+    }
+    );
+    const data = await res.json();
+    console.log(data)
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'> 
       <h1 className='text-3xl font-semibold text-center my-10'>
         Sign Up
       </h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" name="Username" id="username" placeholder='Username' className='border rounded-lg p-3' required/>
-        <input type="text" name="email" id="emial" placeholder='Email' className='border rounded-lg p-3'required/>
-        <input type="text" name="password" id="password" placeholder='Password' className='border rounded-lg p-3' required/>
+      <form onSubmit = {handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" name="Username" id="username" placeholder='Username' className='border rounded-lg p-3'  onChange={handleChange}/>
+        <input type="email" name="email" id="emial" placeholder='Email' className='border rounded-lg p-3' onChange={handleChange}/>
+        <input type="password" name="password" id="password" placeholder='Password' className='border rounded-lg p-3'  onChange={handleChange}/>
         <button className='bg-blue-500 text-white font-semibold p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'>Sign Up</button>
         <button className='bg-red-700 text-white font-semibold p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'>Continue With Google</button>
       </form>
